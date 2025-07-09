4c12889ae0c59ac90027617dd07198d94e97f5a1
Alexander Koskovich <akoskovich@pm.me>
2025-07-09 22:04:54 +1000

    misc: haptic: Add a short delay before entering stream mode
    Change-Id: I944c2858ae1c2efb1b38a645af2f3e3c110d9486

--- modules/drivers/misc/haptic/haptic_drv.c
+++ modules/drivers/misc/haptic/haptic_drv.c
@@ -1115,6 +1115,8 @@ static void preset_work_routine(struct work_struct *work)
 	haptic_data->func->set_play_mode(haptic_data, PLAY_MODE_STREAM);
 	haptic_data->func->play_go(haptic_data);
 
+	usleep_range(1000, 2000);
+
 	send_stream_data(haptic_data, chip_fifo_size);
 	mutex_unlock(&haptic_data->lock);
 }
